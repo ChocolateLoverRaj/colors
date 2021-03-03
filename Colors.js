34c4110d0846e18aa65ec6d778fb240b06547412
KhajasJS <pararaj25@pewaukeeschools.org>
2021-03-03 19:46:58 +0000

    tick()

--- Colors.js
+++ Colors.js
@@ -1,6 +1,45 @@
 import Game from '../lib/Game.js'
 
+const Directions = {
+  LEFT: 0,
+  RIGHT: 1
+}
+
 class Colors extends Game {
+  constructor () {
+    super()
+
+    this.state = {
+      x: 0,
+      direction: Directions.RIGHT,
+      speed: 5
+    }
+
+    window.onclick = () => {
+      if (this.tickInterval === undefined) {
+        this.play()
+      } else {
+        this.pause()
+      }
+    }
+  }
+
+  tick () {
+    if (this.state.direction === Directions.RIGHT) {
+      this.state.x += this.state.speed
+      if (this.state.x > 320) {
+        this.state.x = 320
+        this.state.direction = Directions.LEFT
+      }
+    } else {
+      this.state.x -= this.state.speed
+      if (this.state.x < 0) {
+        this.state.x = 0
+        this.state.direction = Directions.RIGHT
+      }
+    }
+  }
+
   render () {
     super.render()
     this.ctx.clearRect(0, 0, 640, 360)
@@ -12,7 +51,7 @@ class Colors extends Game {
     this.ctx.fillRect(320, 0, 320, 160)
 
     this.ctx.fillStyle = 'blue'
-    this.ctx.fillRect(0, 160, 320, 160)
+    this.ctx.fillRect(this.state.x, 160, 320, 160)
   }
 }
 
